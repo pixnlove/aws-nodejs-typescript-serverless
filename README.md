d51c1c4f02c28a985da6edf7574cd2cc91a5c8b4
Hugo Fargues <hugo.fargues@gmail.com>
2020-04-03 00:04:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Serverless Starter with NodeJS & Typescript on AWS
+
+## Running the app
+
+Install the [Serverless](https://serverless.com/) framework globally
+```
+npm install serverless -g
+```
+Once done, you can run:
+```
+serverless install -u https://github.com/pixnlove/aws-nodejs-typescript-serverless
+```
---
+++ handler.ts
@@ -0,0 +1,15 @@
+// handler.ts
+import { APIGatewayEvent, Context, Handler, Callback } from 'aws-lambda';
+
+export const hello : Handler = (event : APIGatewayEvent, context : Context, cb : Callback) => {
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!' ,
+      input: event,
+    }),
+  };
+
+  cb(null, response);
+}
